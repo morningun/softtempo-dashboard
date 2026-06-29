e1e3899740c6b62a23c06a90bb5f3daabc45ef6d
kimsungphil <sinsiru57@gmail.com>
2026-06-29 13:00:33 +0900

    feat: 웨이브폼 애니메이션 FFmpeg 연동

--- app.js
+++ app.js
@@ -93,8 +93,7 @@ function showPage(name, el) {
       realBtn.onclick = startGenerate; startGenerate
     }
     //document.getElementById('gen-btn').style.display = 'block';
-    document.getElementById('import-image').src = d.imageDataUrl;
-    document.getElementById('import-image-clean').src = imageDataUrl;
+    document.getElementById('import-image').src = d.imageDataUrl;    
     document.getElementById('import-style').innerText = d.stylePrompt;
     document.getElementById('import-title').innerText = d.title;
     document.getElementById('import-lyrics').innerText = d.lyrics;
